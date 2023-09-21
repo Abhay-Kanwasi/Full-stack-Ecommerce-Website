0dc18eea5f018abbadc9da7469009f04c4b5d400
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-09-21 18:06:51 +0530

    media root added

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from app import views
+from django.conf.urls.static import static
+from django.conf import settings 
+
 urlpatterns = [
     path('', views.home),
     path('product-detail/', views.product_detail, name='product-detail'),
@@ -13,4 +16,4 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('registration/', views.customerregistration, name='customerregistration'),
     path('checkout/', views.checkout, name='checkout'),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
