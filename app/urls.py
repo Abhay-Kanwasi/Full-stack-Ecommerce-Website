1ee0a3157954909bea0bd3fdafbaba77219e9215
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-09-23 23:43:14 +0530

    change password functionality added

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from app import views
 from django.conf.urls.static import static
 from django.conf import settings 
 from django.contrib.auth import views as auth_view
-from .forms import LoginForm
+from .forms import LoginForm, MyPasswordChangeForm
 
 urlpatterns = [
     path('', views.ProductView.as_view(), name='home'),
@@ -13,11 +13,11 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('address/', views.address, name='address'),
     path('orders/', views.orders, name='orders'),
-    path('changepassword/', views.change_password, name='changepassword'),
     path('mobile/', views.mobile, name='mobile'),
     path('mobile/<slug:data>/', views.mobile, name='mobiledata'),
     path('accounts/login/', auth_view.LoginView.as_view(template_name="app/login.html", authentication_form = LoginForm), name='login'),
     path('logout/', auth_view.LogoutView.as_view(next_page= 'login'), name="logout"),
+    path('passwordchange/', auth_view.PasswordChangeView.as_view(template_name = 'app/passwordchange.html', form_class=MyPasswordChangeForm), name='passwordchange'),
     path('registration/', views.CustomerRegistrationView.as_view(), name="customerregistration"),
     path('checkout/', views.checkout, name='checkout'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -41,9 +41,6 @@ def address(request):
 def orders(request):
  return render(request, 'app/orders.html')
 
-def change_password(request):
- return render(request, 'app/changepassword.html')
-
 def mobile(request):
  return render(request, 'app/mobile.html')
 
